86cd361c30ae72ad5333a7ff9a522a927631f83d
Patryk <patrykwidlazb@gmail.com>
2024-09-07 17:54:30 +0200

    init

---
+++ Client/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <div className="card">Test</div> */}
+      <section>
+        <p className="h-screen bg-dark-theme">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea,
+          repudiandae. Veniam, eaque rerum. Harum consectetur dolor esse
+          distinctio enim reprehenderit, vero, consequatur iusto voluptatum
+          fugiat quos veniam dolores temporibus magni?
+        </p>
+      </section>
+    </>
+  );
+}
+
+export default App;
