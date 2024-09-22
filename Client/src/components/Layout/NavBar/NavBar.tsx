f7bd430a82f8a6766dbe74eac0caaf39a99d1464
Patryk <patrykwidlazb@gmail.com>
2024-09-22 17:52:55 +0200

    made mobile menu hide when clicked outside of it

--- Client/src/components/Layout/NavBar/NavBar.tsx
+++ Client/src/components/Layout/NavBar/NavBar.tsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../../../context/AuthProvider";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const NavBar = () => {
   const { isLoggedIn, login, logout, signup } = useAuth();
   const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => setIsOpen(!isOpen);
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className="bg-gray-800 p-4">
       <div className="flex justify-between items-center w-full">
-        {/* Logo */}
         <div className="text-3xl font-bold text-yellow-400 hover:text-white hover:font-bold hover:underline transition duration-200">
           Just Jaunt
         </div>
@@ -57,6 +72,7 @@ const NavBar = () => {
 
         {/* mobile menu */}
         <div
+          ref={menuRef}
           className={`fixed top-0 right-0 h-full w-64 bg-gray-900 text-white transform ${
             isOpen ? "translate-x-0" : "translate-x-full"
           } transition-transform duration-300 ease-in-out z-50`}
