b0f682ad40cc4cc5996de87336feee8ab621c784
Patryk <patrykwidlazb@gmail.com>
2024-10-09 22:04:18 +0200

    map style

--- Client/src/components/Map/Map.tsx
+++ Client/src/components/Map/Map.tsx
@@ -1,10 +1,9 @@
-import "./styles.css";
+import "./Map.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 import { Icon } from "leaflet";
 import { AmadeusActivity, AmadeusHotel } from "../../api/Amadeus";
-import { BsTriangle } from "react-icons/bs";
 
 const customIcon = new Icon({
   iconUrl: "https://cdn-icons-png.flaticon.com/512/447/447031.png",
@@ -17,12 +16,15 @@ interface MapProps {
 
 const Map = ({ markers }: MapProps) => {
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
+    <MapContainer
+      style={{ height: "90vh", width: "90%", borderRadius: "2rem" }}
+      center={[48.8566, 2.3522]}
+      zoom={13}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
       {markers.map((marker, index) => (
         <Marker
           key={index}
