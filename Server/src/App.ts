73e563d174e3cd4d1e856756f8b0af9bfe9df106
Patryk <patrykwidlazb@gmail.com>
2025-01-05 14:36:47 +0100

    wip

---
+++ Server/src/App.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { connectDB } from "./dbCon";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+connectDB()
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB:", error);
+  });
--- Server/src/dbCon.ts
+++ Server/src/dbCon.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+import { seedDatabase } from "./seeds/seedDatabase";
 
 const DB_CONN_STRING = "mongodb://0.0.0.0:27017/";
 const DB_NAME = "JustJauntDB";
@@ -12,6 +13,8 @@ export const connectDB = async () => {
   try {
     await mongoose.connect(mongoUri);
     console.log("MongoDB connected successfully");
+
+    await seedDatabase();
   } catch (error) {
     console.error("MongoDB connection error:", error);
     throw new Error("Failed to connect to MongoDB");
