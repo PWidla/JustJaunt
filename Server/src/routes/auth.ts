26e33add99cbd50c6bbe54cbfebee7676f8382ec
Patryk <patrykwidlazb@gmail.com>
2025-01-11 19:49:41 +0100

    auth router

---
+++ Server/src/routes/auth.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response } from "express";
+import { User } from "../models/user";
+
+const router = express.Router();
+
+router.post("/login", async (req: Request, res: Response): Promise<any> => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    console.log(user);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    if (user.password !== password) {
+      return res.status(401).json({ error: "Incorrect password" });
+    }
+
+    return res.status(200).json({
+      user: {
+        id: user._id,
+        username: user.username,
+      },
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+export default router;
