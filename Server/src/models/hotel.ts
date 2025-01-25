7e6701c104e04b9f7b2e95ea99bc8aee2149896f
Patryk <patrykwidlazb@gmail.com>
2025-01-25 15:14:11 +0100

    hotel schema

---
+++ Server/src/models/hotel.ts
@@ -0,0 +1,18 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface IHotel extends Document {
+  entityId: string;
+  name: string;
+  geoCode: { latitude: number; longitude: number };
+}
+
+const HotelSchema: Schema = new Schema({
+  entityId: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  geoCode: {
+    latitude: { type: Number, required: true },
+    longitude: { type: Number, required: true },
+  },
+});
+
+export const Hotel = mongoose.model<IHotel>("Hotel", HotelSchema);
